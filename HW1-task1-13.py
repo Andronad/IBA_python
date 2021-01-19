90dc11b99e02a9906644556cf925782c0d33a120
Andronad <andronad.am@gmail.com>
2021-01-19 17:24:01 +0300

    Initial commit

--- HW1-task1-13.py
+++ HW1-task1-13.py
@@ -4,10 +4,10 @@ if k < 1:
     print('Число не натуральное')
 else:
     if k % 10 == 1 and k % 100 != 11:
-        print(str(k) + ' гриб')
+        print('Мы нашли '+str(k) + ' гриб в лесу')
     elif (k % 10 == 2 and k % 100 != 12) or \
             (k % 10 == 3 and k % 100 != 13) or \
             (k % 10 == 4 and k % 100 != 14):
-        print(str(k) + ' гриба')
+        print('Мы нашли '+str(k) + ' гриба в лесу')
     else:
-        print(str(k) + ' грибов')
+        print('Мы нашли '+str(k) + ' грибов в лесу')
