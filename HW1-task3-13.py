e0d0730c8ce9c5d23aebac3becb3e62af8e2282f
Andronad <andronad.am@gmail.com>
2021-01-19 17:52:10 +0300

    task 3

---
+++ HW1-task3-13.py
@@ -0,0 +1,13 @@
+import random
+
+n = int(input('Введите n\n'))
+if n > 1:
+    A = [random.randint(0, 99) for i in range(0, n)]
+    print(A)
+    B = []
+    sum_of_prev = 0
+    for i in range(0, len(A)):
+        if i != 0:
+            sum_of_prev += A[i - 1]
+        B.append(sum_of_prev)
+    print(B)
